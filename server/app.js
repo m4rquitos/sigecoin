13836d7029cc3a1388ccc33e8c9bc8bfe47b611a
JoseRamirez <joseramirezgomez00@gmail.com>
2023-11-10 09:12:04 -0500

    implementacion products

--- server/app.js
+++ server/app.js
@@ -1,28 +1,40 @@
 const express = require('express');
-const { API_VERSION } = require("./constants");
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+const { API_VERSION } = require('./constants');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 
-const app = express()
+// Importar rutas
+const authRoutes = require('./router/auth.router');
+const userRoutes = require('./router/user.router');
+const menuRoutes = require('./router/menu.router');
+const productRoute = require('./router/user.router');
 
-//importar rutas
-const authRoutes = require("./router/auth.router")
-const userRoutes = require("./router/user.router")
-const menuRoutes = require("./router/menu.router")
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(fileUpload());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
 
-// configurar body parse
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// conf carpeta static
-app.use(express.static("uploads"))
-//conf header HTTP -cors
-app.use(cors())
-//conf rutas
-app.use(`/api/${API_VERSION}`, authRoutes)
-app.use(`/api/${API_VERSION}`, userRoutes)
-app.use(`/api/${API_VERSION}`, menuRoutes)
+app.use('/api', productRoute);
 
+app.use(express.static('uploads'));
+app.use(cors());
 
-module.exports = app
+app.use(`/api/${API_VERSION}`, authRoutes);
+app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, menuRoutes);
+
+module.exports = app;
