18c6c9ae9b98207194ebde9cf9040645ddcc1db6
Diomedez <diomedesd732@gmail.com>
2023-11-22 09:13:16 -0500

    crud-completo-productos

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -45,4 +45,94 @@ async function registerProduct(req, res) {
     }
 }
 
-module.exports = { registerProduct };
+const getProducts = async (req, res) => {
+    try{
+        const data = await Producto.find()
+        res.status(200).json(data)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+async function updateProduct(req, res) {
+    const { codigoProduct } = req.params;
+    const { nombreProduct, precioUni, tallas, categorias, tipoCalzado, descripcion } = req.body;
+    const { file } = req.files; // file contendrá la información del archivo enviado
+
+    try {
+        // Verificar si el producto existe
+        const product = await Producto.findOne({ codigoProduct });
+        if (!product) {
+            return res.status(404).json({ msg: 'Product not found' });
+        }
+
+        // Actualizar los campos del producto
+        product.nombreProduct = nombreProduct || product.nombreProduct;
+        product.precioUni = precioUni || product.precioUni;
+        product.tallas = tallas || product.tallas;
+        product.categorias = categorias || product.categorias;
+        product.tipoCalzado = tipoCalzado || product.tipoCalzado;
+        product.descripcion = descripcion || product.descripcion;
+
+        // Actualizar la imagen si se proporcionó un nuevo archivo
+        if (file) {
+            // Eliminar el archivo anterior
+            const filePath = path.resolve(__dirname, '..', 'uploads', product.images);
+            fs.unlinkSync(filePath);
+
+            // Construir la ruta del nuevo archivo con el nombre original del archivo
+            const rutaArchivoRelativa = path.join( '..', 'uploads' , 'productos', codigoProduct, file.name);
+            const rutaSinUpload = path.join('productos', codigoProduct, file.name);
+
+            // Guardar el nuevo archivo
+            fs.writeFileSync(path.resolve(__dirname, rutaArchivoRelativa), file.data);
+
+            // Actualizar la ruta de la imagen en el producto
+            product.images = rutaSinUpload;
+        }
+
+        // Guardar el producto actualizado en la base de datos
+        const updatedProduct = await product.save();
+        res.status(200).json({ msg: 'Product updated successfully', updatedProduct });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Internal Server Error' });
+    }
+}
+
+async function deleteProduct(req, res) {
+    const { codigoProduct } = req.params;
+
+    try {
+        // Buscar el producto por su código
+        const product = await Producto.findOne({ codigoProduct });
+        if (!product) {
+            return res.status(404).json({ msg: 'Product not found' });
+        }
+
+        // Obtener la ruta del directorio
+        const directoryPath = path.resolve(__dirname, '..', 'uploads', 'productos', codigoProduct);
+
+        // Eliminar el archivo asociado al producto
+        const filePath = path.resolve(directoryPath, '..', '..', product.images);
+        fs.unlinkSync(filePath);
+
+        // Eliminar el directorio
+        await fs.promises.rm(directoryPath, { recursive: true });
+
+        // Eliminar el producto de la base de datos
+        await product.remove();
+
+        res.status(200).json({ msg: 'Product deleted successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Internal Server Error' });
+    }
+}
+
+module.exports = {
+    registerProduct,
+    getProducts,
+    updateProduct,
+    deleteProduct
+};
--- server/router/product.routes.js
+++ server/router/product.routes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
-const { registerProduct } = require('../controllers/product.controller.js');
+const { registerProduct, getProducts, updateProduct, deleteProduct } = require('../controllers/product.controller.js');
 
 const api = Router();
 
 api.post('/registerProduct', registerProduct);
+api.get('/getProducts', getProducts);
+api.patch('/updateProduct/:codigoProduct', updateProduct);
+api.delete('/deleteProduct/:codigoProduct', deleteProduct);
 
 module.exports = api;
